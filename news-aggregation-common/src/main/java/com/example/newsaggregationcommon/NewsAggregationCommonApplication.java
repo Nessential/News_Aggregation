5dc46d0fbe75a5d2e6d22e4dacfea3e5914f0a0f
zzh <3206742522@qq.com>
2025-12-25 17:40:12 +0800

    [feat] 初始化springboot项目

---
+++ news-aggregation-common/src/main/java/com/example/newsaggregationcommon/NewsAggregationCommonApplication.java
@@ -0,0 +1,13 @@
+package com.example.newsaggregationcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregationCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsAggregationCommonApplication.class, args);
+    }
+
+}
